0658fc7be509f2a0f6eb5807dfa8822045ac4c89
Mirsaid <akbaraliyev522@mail.ru>
2024-08-05 13:27:12 +0500

    main

--- js/app.js
+++ js/app.js
@@ -1,6 +1,113 @@
-let son = -12;
+// Misol-1
 
-if(son > 0){
-    son =+1;
-    console.log(son);
-}
+// let son = prompt("Son kiriting");
+
+// son = Number(son);
+
+// if (son > 0) {
+//     son =+ 1;
+// }
+
+// console.log("Natija" + son);
+
+// Misol-2
+
+// let son = prompt("Son kiriting");
+
+// son = Number(son);
+
+// if (son > 0) {
+//     son += 1;
+// } else {
+//     son -= 2;
+// }
+
+// console.log("Natija" + son);
+
+// Misol-3
+
+// let son = prompt("Son kiriting");
+
+// son = Number(son);
+
+// if( son > 0){
+//     son += 1;
+// }else if(son === 0){
+//     son = 10;
+// }else{
+//     son -= 2;
+// }
+// console.log("Natija"+son);
+
+// Misol-4
+
+// let son = prompt("Birinchi sonni kiriting:");
+// let son2 = prompt("Ikkinchi sonni kiriting:");
+// let son3 = prompt("Uchinchi sonni kiriting:");
+
+// son = Number(son);
+// son2 = Number(son2);
+// son3 = Number(son3);
+
+// let musbat = 0;
+
+// if (son > 0) {
+//     musbat++;
+// }
+
+// if (son2 > 0) {
+//     musbat++;
+// }
+
+// if (son3 > 0) {
+//     musbat++;
+// }
+
+// console.log("Musbat sonlar soni: " + musbat);
+
+
+// Misol-5
+
+// let son = prompt("Birinchi sonni kiriting:");
+// let son2 = prompt("Ikkinchi sonni kiriting:");
+// let son3 = prompt("Uchinchi sonni kiriting:");
+
+// son = Number(son);
+// son2 = Number(son2);
+// son3 = Number(son3);
+
+// let musbat = 0;
+// let manfiy = 0;
+
+// if (son > 0) {
+//     musbat++;
+// } else if (son < 0) {
+//     manfiy++;
+// }
+
+// if (son2 > 0) {
+//     musbat++;
+// } else if (son2 < 0) {
+//     manfiy++;
+// }
+
+// if (son3 > 0) {
+//     musbat++;
+// } else if (son3 < 0) {
+//     manfiy++;
+// }
+
+// console.log("Musbat sonlar soni: " + musbat);
+// console.log("Manfiy sonlar soni: " + manfiy);
+
+
+// Misol-6
+
+// let son = 10;
+// let son2 = 12;
+
+// if(son > son2 && son2 > son){
+//     console.log(son);
+// }else{
+//     console.log(son2);
+// }
